ac8b9cc86f9726e2e8e74ea482a1a1557ff710ab
Chung Nguyen <chung.ndinh.2309@gmail.com>
2021-08-07 20:44:01 +0700

    updated branch

--- webapp/StaffTest.php
+++ webapp/StaffTest.php
@@ -11,9 +11,9 @@ class StaffTest extends TestCase
         $staff = new Staff;
 
         $staff->first_name = "Nguyen";
-        $staff->last_name = "Dinh Chung 3";
+        $staff->last_name = "Dinh Chung 2";
 
-        $this->assertEquals('Nguyen Dinh Chung 3', $staff->getFullName());
+        $this->assertEquals('Nguyen Dinh Chung 2', $staff->getFullName());
     }
     public function testFullNameIsEmptyByDefault()
     {
